c242820a5def1fa2dc5182868fab4defdf6bba7a
David Voong <voong.david@gmail.com>
2015-02-01 10:00:48 -0500

    Added a tag filtering system

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'vocab',
     'myproject',
+    'friendship',
 )
 
 MIDDLEWARE_CLASSES = (
--- vocab/views.py
+++ vocab/views.py
@@ -1,6 +1,7 @@
 import collections
 import json
 import django
+from friendship.models import Friend, Follow
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
@@ -9,6 +10,54 @@ from vocab.models import Word, Comment, Tag
 from django.contrib.auth.models import User
 
 # Create your views here.
+
+def friendship_test(request):
+    user = request.user
+    all_friends = Friend.objects.friends(request.user)
+    return HttpResponse(str(all_friends))
+
+    # # List all unread friendship requests
+    # requests = Friend.objects.unread_requests(user=request.user)
+
+    # # List all rejected friendship requests
+    # rejects = Friend.objects.rejected_requests(user=request.user)
+
+    # # List all unrejected friendship requests
+    # unrejects = Friend.objects.unrejected_requests(user=request.user)
+
+    # # List all sent friendship requests
+    # sent = Friend.objects.sent_requests(user=request.user)
+
+    # # List of this user's followers
+    # all_followers = Following.objects.followers(request.user)
+
+    # # List of who this user is following
+    # following = Following.objects.following(request.user)
+
+    # ### Managing friendship relationships
+
+    # # Create a friendship request
+    # other_user = User.objects.get(pk=1)
+    # new_relationship = Friend.objects.add_friend(request.user, other_user)
+
+    # # Can optionally save a message when creating friend requests
+    # message_relationship = Friend.objects.add_friend(
+    #     from_user=request.user,
+    #     to_user=some_other_user,
+    #     message='Hi, I would like to be your friend',
+    # )
+
+    # # And immediately accept it, normally you would give this option to the user
+    # new_relationship.accept()
+
+    # # Now the users are friends
+    # Friend.objects.are_friends(request.user, other_user) == True
+
+    # # Remove the friendship
+    # Friend.objects.remove_friend(other_user, request.user)
+
+    # # Create request.user follows other_user relationship
+    # following_created = Following.objects.add_follower(request.user, other_user)
 
 def build_comment_dict(comment):
     comment_dict = {
